45298e051984a7a4150cffa0998967fefd28b13a
Thomson <thomson470@gmail.com>
2024-12-18 15:47:26 +0700

    Ganti dari .env ke static.js

--- src/scripts/static.js
+++ src/scripts/static.js
@@ -1,16 +1,16 @@
 // diisi / direplace di sisi server
 const STATIC = {
-    "__app__": {
+    "__admin__": {
         id: "",
         title: "",
-        web: "",
-        api: "http://localhost:5401/_/api",
-        //web: "/_/web",
-        //api: "/_/api",
+        //web: "",
+        //api: "http://localhost:5401/_/api",
+        web: "/_/web",
+        api: "/_/api",
         timeout: 60,
         language: "en",
         debug: true,
     }
 };
-const APP = STATIC["__app__"];
+const APP = STATIC["__admin__"];
 export { APP };
