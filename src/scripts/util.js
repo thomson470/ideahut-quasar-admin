83720ae6c978f52edf3a6fc1e4dc6028cab851d8
Ideahut <ideahut.apps.team@gmail.com>
2025-05-13 01:17:30 +0700

    fix util format money bug

--- src/scripts/util.js
+++ src/scripts/util.js
@@ -133,14 +133,16 @@ const util = {
    */
   format: {
     /* MONEY */
-    money(number, thousand, decimal, places, symbol) {
-      places = !isNaN((places = Math.abs(places))) ? places : 2
-      symbol = symbol !== undefined ? symbol : ''
-      thousand = thousand || '.'
-      decimal = decimal || ','
-      let negative = number < 0 ? '-' : '',
-        i = parseInt((number = Math.abs(+number || 0).toFixed(places)), 10) + '',
-        j = (j = i.length) > 3 ? j % 3 : 0
+    money(number, options) {
+      let opt = util.isObject(options) ? options : {}
+      let places = Math.abs(opt.places)
+      places = !isNaN(places) ? places : 2
+      let symbol = util.isString(opt.symbol) ? opt.symbol : ''
+      let thousand = util.isString(opt.thousand) ? opt.thousand : '.'
+      let decimal = util.isString(opt.decimal) ? opt.decimal : ','
+      let negative = number < 0 ? '-' : ''
+      let i = parseInt((number = Math.abs(+number || 0).toFixed(places)), 10) + ''
+      let j = i.length > 3 ? i.length % 3 : 0
       return (
         symbol +
         negative +
